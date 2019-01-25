e1ff800a2efcf31620c05da61a772e1150ff4449
takeaki ishizuka <takewatage.meily@gmail.com>
2019-01-26 02:05:13 +0900

    Schema 追加

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+<AppHeader></AppHeader>
     <transition name="page" mode="out-in">
       <slot>
         <router-view></router-view>
@@ -9,11 +10,17 @@
 </template>
 
 <script>
+    import AppHeader from "./Organisms/AppHeader.vue";
+
     export default {
         el: '#app',
 
         mounted() {
             console.log('Component mounted.')
+        },
+
+        components: {
+            AppHeader
         }
     }
 </script>
--- resources/js/components/Atoms/TitleLogo.vue
+++ resources/js/components/Atoms/TitleLogo.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <a class="flex-center title-logo" href="/">
-            KoDo
+            Laravel SPA
         </a>
     </div>
 </template>
